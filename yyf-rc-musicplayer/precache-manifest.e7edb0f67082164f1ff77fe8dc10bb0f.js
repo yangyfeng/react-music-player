d2fd4d625986d4d8481e7bc4202eaaafea7d3f32
yangyufeng <1539558157@qq.com>
2018-11-15 11:05:40 +0800

    提交打包的文件git 托管

---
+++ yyf-rc-musicplayer/precache-manifest.e7edb0f67082164f1ff77fe8dc10bb0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6379fbf8df8acf95e7d",
+    "url": "/static/js/main.a6379fbf.chunk.js"
+  },
+  {
+    "revision": "beb863f2009ca21dad9c",
+    "url": "/static/js/1.beb863f2.chunk.js"
+  },
+  {
+    "revision": "a6379fbf8df8acf95e7d",
+    "url": "/static/css/main.ac415dde.chunk.css"
+  },
+  {
+    "revision": "780d4fad0a6ace2be764cef8bb3be147",
+    "url": "/index.html"
+  }
+];
